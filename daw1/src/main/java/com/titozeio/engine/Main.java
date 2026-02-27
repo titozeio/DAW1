af1663257baaee687b094a233e3542af510141ca
titozeio <sergiomontesreepiso@gmail.com>
2026-02-28 00:17:58 +0100

    added Main class

---
+++ daw1/src/main/java/com/titozeio/engine/Main.java
@@ -0,0 +1,27 @@
+package com.titozeio.engine;
+
+// Main.java
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Game game;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Devastation AI Wars 1");
+        primaryStage.setWidth(1280);
+        primaryStage.setHeight(720);
+
+        // Inicializa el juego pasándole la ventana
+        this.game = new Game(primaryStage);
+        this.game.start();
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
