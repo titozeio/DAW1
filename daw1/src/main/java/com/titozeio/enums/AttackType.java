b52bfeb4ecfabbad0f78bbe5c6e101e5b89e694e
titozeio <sergiomontesreepiso@gmail.com>
2026-02-27 23:47:15 +0100

    Enums #13

---
+++ daw1/src/main/java/com/titozeio/enums/AttackType.java
@@ -0,0 +1,14 @@
+package com.titozeio.enums;
+
+/**
+ * Tipos de ataque de las armas (mencionado en el GDD como OMSI).
+ *
+ * ENERGY - Armas de energía (cañones de plasma, láser, rayos).
+ * EXPLOSIVE - Armas explosivas (misiles, granadas).
+ * KINETIC - Armas cinéticas (cañones automáticos, martillos).
+ */
+public enum AttackType {
+    ENERGY,
+    EXPLOSIVE,
+    KINETIC
+}
