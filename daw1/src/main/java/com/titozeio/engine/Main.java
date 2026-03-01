084fc0f704fee0a0bee12a6cb526146116b52133
titozeio <sergiomontesreepiso@gmail.com>
2026-03-01 19:44:40 +0100

    font exo2 restored

--- daw1/src/main/java/com/titozeio/engine/Main.java
+++ daw1/src/main/java/com/titozeio/engine/Main.java
@@ -4,8 +4,6 @@ package com.titozeio.engine;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
-import java.io.File;
-import java.net.MalformedURLException;
 
 public class Main extends Application {
 
@@ -14,11 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         // Cargar fuente personalizada
-        try {
-            Font.loadFont(new File("assets/fonts/Exo2-Regular.ttf").toURI().toURL().toString(), 14);
-        } catch (MalformedURLException e) {
-            System.err.println("No se pudo cargar la fuente: " + e.getMessage());
-        }
+        Font.loadFont(Main.class.getResourceAsStream("/com/titozeio/fonts/Exo2-Regular.ttf"), 14);
 
         primaryStage.setTitle("Devastation AI Wars 1");
         primaryStage.setWidth(1280);
